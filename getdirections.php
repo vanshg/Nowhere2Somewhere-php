d917f56efa644dc23dc9ccb136b4532de830b857
vanshg <vansh.gandhi@gmail.com>
2014-05-25 08:56:32 -0700

    initial commit

--- getdirections.php
+++ getdirections.php
@@ -1,13 +1,15 @@
 <?php
     require "Services/Twilio.php";
-    //$body =
+    $to = $_POST['From'];
     $AccountSid = "AC9c9ffe78169960383ddcbd1fac9bbf3d";
     $AuthToken = "dd7f067bdb806e2a9a5f9c0e35f52046";
     $client = new Services_Twilio($AccountSid, $AuthToken);
     $from = '7076745678';
     $people = array("+19253886379" => "Vansh", "+19259807321" => "Carl");
-    $start = '7228 Moss Tree Way, Pleasanton, CA 94566';
-    $finish = '773 Summit Creek Lane, Pleasanton, CA 94566';
+    $fulltext = $_POST['Body'];
+    $fulltextarray = explode('|', $fulltext, 2);
+    $start = $fulltextarray[0];
+    $finish = $fulltextarray[1];
     $start = urlencode($start);
     $finish = urlencode($finish);
     $url = 'http://dev.virtualearth.net/REST/V1/Routes/Driving?o=xml&wp.0=' . $start . '&wp.1=' . $finish . '&avoid=minimizeTolls&key=AnuSkwrW2ovkGB807r9EjfSnyN7IDeMDCIbpPJiRZxoLhe4Bb5L8_Dbq7PU-VIKb';
@@ -26,12 +28,14 @@
     $result = "";
     $x = 1;
     foreach($Response -> ResourceSets -> ResourceSet -> Resources -> Route -> RouteLeg -> ItineraryItem as $iten)
-        $result .= $x++ . '. ' . $iten->Instruction . "<br>";
+        $result .= $x++ . '. ' . $iten->Instruction . "\r\n";
 
-    header("content-type: text/xml");
-    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+    $array = str_split($result, 160);
+    $arrlen = count($array, COUNT_NORMAL);
+    foreach($array as $string)
+    {
+        $client->account->sms_messages->create($from, $to, $string);
+        sleep(9);
+    }
 ?>
-
-<Response>
-    <Message><?php echo $result ?></Message>
-</Response>
